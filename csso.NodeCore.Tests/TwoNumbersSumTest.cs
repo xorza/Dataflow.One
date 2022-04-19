6ef8ae34eeea01a715bd03c182dc74b9cdc06873
xxorza <xxorza@gmail.com>
2022-04-18 21:48:56 -0700

    test updated

--- csso.NodeCore.Tests/TwoNumbersSumTest.cs
+++ csso.NodeCore.Tests/TwoNumbersSumTest.cs
@@ -28,35 +28,39 @@ public class TwoNumbersSumTest {
                 graph.ConstNode1.Outputs.Single())
         );
 
-        var addToConst2Sub = new DataSubscription(
+        graph.Graph.Add(new DataSubscription(
             graph.AddNode.Inputs[1],
-            graph.ConstNode2.Outputs.Single());
-        graph.Graph.Add(addToConst2Sub);
+            graph.ConstNode2.Outputs.Single())
+        );
 
         var executor = new Executor(graph.Graph);
         graph.FrameNoFunc.Executor = executor;
-
 
         executor.Run();
         Assert.AreEqual(1256, graph.OutputFunc.Value);
 
         var addEvaluationNode = executor.GetEvaluationNode(graph.AddNode);
         var frameNoEvaluationNode = executor.GetEvaluationNode(graph.FrameNoNode);
-        var constEvaluationNode = executor.GetEvaluationNode(graph.ConstNode1);
+        var const1EvaluationNode = executor.GetEvaluationNode(graph.ConstNode1);
         Assert.True(frameNoEvaluationNode.State == EvaluationState.Idle);
-        Assert.True(constEvaluationNode.State == EvaluationState.Invoked);
-        Assert.True(addEvaluationNode.State >= EvaluationState.Invoked);
+        Assert.True(const1EvaluationNode.State == EvaluationState.Invoked);
+        Assert.True(addEvaluationNode.State == EvaluationState.Invoked);
 
-
-        graph.Graph.Remove(addToConst2Sub);
         graph.Graph.Add(
             new DataSubscription(
                 graph.AddNode.Inputs[1],
                 graph.FrameNoNode.Outputs.Single())
         );
-        
+
         executor.Run();
         Assert.AreEqual(4, graph.OutputFunc.Value);
+
+        addEvaluationNode = executor.GetEvaluationNode(graph.AddNode);
+        frameNoEvaluationNode = executor.GetEvaluationNode(graph.FrameNoNode);
+        var const2EvaluationNode = executor.GetEvaluationNode(graph.ConstNode2);
+        Assert.True(frameNoEvaluationNode.State == EvaluationState.Invoked);
+        Assert.True(const2EvaluationNode.State == EvaluationState.Idle);
+        Assert.True(addEvaluationNode.State == EvaluationState.Invoked);
 
 
         Assert.Pass();
